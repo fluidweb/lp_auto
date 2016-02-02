801f94ffcb89c5e281277442fe06462f6eefce9f
dis-zephyro <zephyro@yandex.ru>
2016-02-03 00:45:45 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $('.slider').slick({
     autoplay: true,
     dots: false,
     fade: true,
-    speed: 1000,
+    speed: 800,
     cssEase: 'linear',
     slidesToShow: 1,
     slidesToScroll: 1
@@ -45,3 +45,19 @@ $(".btn-modal").fancybox({
         closeBtn : '<a title="Close" class="btn_close" href="javascript:;"></a>'
     }
 });
+
+
+/* Поле отправки файла */
+
+function getName (str){
+    if (str.lastIndexOf('\\')){
+        var i = str.lastIndexOf('\\')+1;
+    }
+    else{
+        var i = str.lastIndexOf('/')+1;
+    }
+    var filename = str.slice(i);
+    var uploaded = document.getElementById("fileformlabel");
+    uploaded.innerHTML = filename;
+}
+
