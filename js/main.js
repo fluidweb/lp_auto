453bfba4906f4dbb104a95afef5420cef124aadd
dis-zephyro <zephyro@yandex.ru>
2016-02-02 22:27:56 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -16,3 +16,32 @@ $('.slider-nav.prev').click(function(){
 $('.slider-nav.next').click(function(){
     $('.slider').slick('slickNext');
 });
+
+$('.review').slick({
+    arrows: false,
+    autoplay: true,
+    dots: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+
+
+$('.btn-b04').click(function(){
+    var str=$(this).attr('href');
+    $.scrollTo(str, 500);
+    return false;
+});
+
+// ----- Маска ----------
+jQuery(function($){
+    $("input[name='phone']").mask("+7(999) 999-9999");
+});
+
+
+$(".btn-modal").fancybox({
+    'padding'    : 0,
+    'tpl'        : {
+        closeBtn : '<a title="Close" class="btn_close" href="javascript:;"></a>'
+    }
+});
